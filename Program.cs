4f84209c4f0d4d7c65e02b109ae4d89f22d13b87
akarsh <akarshm07@gmail.com>
2021-05-26 22:49:32 +0530

    add file to check if a word is palindrome

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word: ");
+            string abc = Console.ReadLine().ToLower();
+            int count = 0;
+            if (abc.Length > 0)
+            {
+                for (int i = 0; i < abc.Length; i++)
+                {
+                    if (abc[i] == abc[((abc.Length - 1) - i)])
+                    {
+                        count++;
+                    }
+                }
+
+                if (abc.Length == count)
+                {
+                    Console.WriteLine(abc + " is a Palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(abc + " is not a Palindrome");
+                }
+            }
+        }
+    }
+}
